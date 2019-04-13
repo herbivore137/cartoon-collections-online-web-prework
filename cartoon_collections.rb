2ed8f8cc1ddbf32eb0230b3fc6e7085557040675
B.A. Hanlon <herbivore137@gmail.com>
2019-04-13 23:12:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,6 @@ def long_planeteer_calls(calls_array)
 end
 
 def find_the_cheese(str_array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
   cheese_types.each do |cheese|
