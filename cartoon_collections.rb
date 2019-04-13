799c5bd14bd4c61bb6b30ee209a443a463c8102a
B.A. Hanlon <herbivore137@gmail.com>
2019-04-13 23:01:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_array)
-  calls_array.any? do
+  calls_array.any? do |call|
+    call.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
